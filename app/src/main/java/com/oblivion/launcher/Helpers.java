e2ef28945366e1adfdac4b75a191237246db0197
Saumya Jain <samjain0913@gmail.com>
2020-06-10 12:30:21 +0530

    fixing default app now

---
+++ app/src/main/java/com/oblivion/launcher/Helpers.java
@@ -0,0 +1,22 @@
+package com.oblivion.launcher;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Drawable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Helpers {
+    public static Drawable getActivityIcon(Context context, String packageName, String activityName) {
+        PackageManager pm = context.getPackageManager();
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName(packageName, activityName));
+        ResolveInfo resolveInfo = pm.resolveActivity(intent, 0);
+        return resolveInfo.loadIcon(pm);
+    }
+}
